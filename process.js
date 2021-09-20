256c429af535fb1506f4390d0a6d5e1c9f1cd049
Ale Olea <alejandra.olea@globant.com>
2021-09-20 15:48:02 -0300

    Second Commit

--- process.js
+++ process.js
@@ -1,2 +1,45 @@
-let hello = 'hello'
-console.log(hello)
+const { readdir, readFile } = require('fs')
+const { promisify } = require('util')
+
+const validator = (data) => {
+  //   const keys = Object.keys()
+  //   const values = Object.values()
+  //   const entries = Object.entries()
+  return true
+}
+
+// const result = fs.readdirSync('./dataset', { encoding: 'utf-8' })
+// console.log('result', result)
+
+const promisifiedReadDir = promisify(readdir)
+const promisifiedReadFile = promisify(readFile)
+
+async function main() {
+  try {
+    const files = await promisifiedReadDir('./dataset', { encoding: 'utf-8' })
+    console.log(files)
+    for (let file of files) {
+      const fileContent = await promisifiedReadFile(`./dataset/${file}`, { encoding: 'utf-8' })
+      console.log(validator(fileContent))
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+main()
+
+// fs.readdir('./dataset', { encoding: 'utf-8' }, (err, data) => {
+//   if (err) {
+//     console.log('err', error)
+//   }
+//   console.log('data', data)
+
+//   for (let file of data) {
+//     fs.readFile(`./dataset/${file}`, { encoding: 'utf-8' }, (err, data) => {
+//       if (err) {
+//         console.log(err)
+//       }
+//       console.log(data)
+//     })
+//   }
+// })
