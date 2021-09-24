7b55b10d2c04453b2375ca3ae8394da64f1be9b2
Ale Olea <alejandra.olea@globant.com>
2021-09-24 10:19:13 -0300

    iteration 2

---
+++ read.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const { readdir, readFile } = require('fs')
+const promisifiedReadDir = promisify(readdir)
+const promisifiedReadFile = promisify(readFile)
+const validator = require('./validator.js')
+
+async function reader() {
+  try {
+    const files = await promisifiedReadDir('./dataset', { encoding: 'utf-8' })
+    console.log(files)
+    for (let file of files) {
+      const fileContent = await promisifiedReadFile(`./dataset/${file}`, { encoding: 'utf-8' })
+      validator(fileContent)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+module.exports = reader
