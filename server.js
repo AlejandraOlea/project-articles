48d2b8104c51e5588aa693766b8419f3748e67e8
Ale Olea <alejandra.olea@globant.com>
2021-09-27 12:44:22 -0300

    Feat: Server

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
-const ttp = require('http')
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const reader = require('./reader.js')
 
-const requestListener = function (req, res) {
-  res.writeHead(200)
-  res.end('hello')
-}
+const server = http.createServer(async (req, res) => {
+  //set  header content type
+  res.setHeader('Content-type', 'application/json')
+  let data = await reader('./db.json')
+  let queryString = url.parse(req.url, true).query
+  console.log(queryString)
+  switch (true) {
+    case req.url === '/articles':
+      try {
+        res.write(data)
+        res.end()
+        res.stausCode = 200
+        break
+      } catch (err) {
+        console.log(err)
+      }
+    case typeof queryString.id === 'string':
+      const founded = JSON.parse(data).find((e) => e.id === queryString.id)
+      console.log('founded es', founded)
+      if (founded === undefined) {
+        res.write('Not found')
+        res.stausCode = 404
+        res.end()
+        break
+      }
+      res.write(JSON.stringify(founded))
+      res.end()
+      break
+    default:
+      console.log(url.parse(req.url, true).query.id)
+      res.write('Prompt url')
+      res.end()
+      break
+  }
+})
 
-const server = http.createServer(requestListener)
-server.listen(8080)
+server.listen(8080, 'localhost', () => {
+  console.log('====Listening for requests in port 8080====')
+})
