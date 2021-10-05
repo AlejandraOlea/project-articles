c5f69640e1cdb576db50540bfda33efbbb43ce46
Ale Olea <alejandra.olea@globant.com>
2021-10-05 18:32:00 -0300

    Persistencia

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-
 const app = express()
+//llama instanciacion de la base de datos desde database/index.js
+const db = require('./database')
+const router = require('./routes')
+
+console.log('DB==CONEXION===>', db.connection)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-const router = require('./routes')
-
 app.use('/api/v1/articles', router.articlesRouter)
 
 app.listen(8080, () => {
